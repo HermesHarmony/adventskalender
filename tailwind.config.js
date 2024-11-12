ef87a3968c249cd3714adfe570063e7cff049aa2
Jonas <35344671+HermesHarmony@users.noreply.github.com>
2024-11-12 17:40:38 +0100

    Css Layout for new forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["views/**/*.{html,php}", "components/**/*.{html,php}"],
+  content: ["views/**/*.{html,php}", "components/**/*.{html,php}", "app/**/*.{html,php}"],
   theme: {
     extend: {},
   },
